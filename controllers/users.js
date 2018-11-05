4efbaa3376a4c0b17c58ce09a2a87561660ab81e
Daniel Marczydło <daniel.marczydlo@gmail.com>
2018-11-05 11:24:26 +0100

    remove unnecessary

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,6 @@ const { getUsers } = require(path.resolve('managers/users'));
 
 
 module.exports.get = async (req, res) => {
-    const { date } = req.params;
-    getUsers(date).then(users => returnSuccess(res, users)
+    getUsers().then(users => returnSuccess(res, users)
     ).catch(error => returnError(res, error));
 };
